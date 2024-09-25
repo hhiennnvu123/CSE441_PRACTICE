700549ce4366d510548a2fcd83df6e582e8a2def
hhiennnvu123 <hienv4434@gmail.com>
2024-09-25 17:14:50 +0700

    update2

--- Ex02/app/src/main/java/com/example/ex02/MainActivity.java
+++ Ex02/app/src/main/java/com/example/ex02/MainActivity.java
@@ -2,8 +2,8 @@ package com.example.ex02;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,23 +13,63 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     Button btncall;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "CR424 - onStart()", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "CR424 - onStop()", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "CR424 - onDestroy()", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "CR424 - onPause()", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "CR424 - onRestart()", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "CR424 - onResume()", Toast.LENGTH_LONG).show();
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         btncall = findViewById(R.id.btncall);
-        btncall.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Sub1Activity.class);
-                startActivity(intent);
-            }
+        btncall.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, Sub1Activity.class);
+            startActivity(intent);
         });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
