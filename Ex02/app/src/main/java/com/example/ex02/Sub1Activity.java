8632401dc8520e03dfd729c1c092401ee9368356
hhiennnvu123 <hienv4434@gmail.com>
2024-09-25 15:23:13 +0700

    apdate1

--- Ex02/app/src/main/java/com/example/ex02/Sub1Activity.java
+++ Ex02/app/src/main/java/com/example/ex02/Sub1Activity.java
@@ -1,6 +1,5 @@
 package com.example.ex02;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,19 +10,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    Button btncall;
+public class Sub1Activity extends AppCompatActivity {
+
+    Button btnok1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        btncall = findViewById(R.id.btncall);
-        btncall.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_sub1);
+
+        btnok1 = findViewById(R.id.btnok1);
+        btnok1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Sub1Activity.class);
-                startActivity(intent);
+                finish();
             }
         });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
