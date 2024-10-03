0603244ad7d118a4675fb35a12b28ddabdc61311
hhiennnvu123 <hienv4434@gmail.com>
2024-10-03 18:32:35 +0700

    update3

--- Ex04/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Ex04/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,22 +12,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
     Button btncf, btnfc;
     EditText edtdoC, edtdoF;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         edtdoC = findViewById(R.id.edtdoC);
-        edtdoF = findViewById(R.id.edtdoF); btncf = findViewById(R.id.btncf); btnfc = findViewById(R.id.btnfc);
+        edtdoF = findViewById(R.id.edtdoF);
+        btncf = (Button) findViewById(R.id.btncf);
+        btnfc = (Button) findViewById(R.id.btnfc);
+
         btncf.setOnClickListener(new View.OnClickListener() { @Override
         public void onClick(View v) {
-// TODO Auto-generated method stub DecimalFormat dcf=new DecimalFormat("#.00"); String doC = edtdoC.getText()+"";
-//
-            int C=Integer.parseInt(doC); edtdoF.setText(""+dcf.format(C*1.8+32));;;;
+        DecimalFormat dcf=new DecimalFormat("#.00");
+        String doC = edtdoC.getText()+"";
+            int C=Integer.parseInt(doC); edtdoF.setText(""+dcf.format(C*1.8+32));
 
         }
         });
@@ -34,7 +41,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 DecimalFormat dcf=new DecimalFormat("#.00");
-// TODO Auto-generated method stub String doF = edtdoF.getText()+""; int F=Integer.parseInt(doF);
+                String doF = edtdoF.getText()+"";
+                int F=Integer.parseInt(doF);
                 edtdoC.setText(""+dcf.format((F-32)/1.8));
             }
         });
