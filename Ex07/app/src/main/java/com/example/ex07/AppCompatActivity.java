4aef9f5c5159c4b990a14814c6ff40572d1dd3d0
hhiennnvu123 <hienv4434@gmail.com>
2024-10-10 01:03:09 +0700

    done

---
+++ Ex07/app/src/main/java/com/example/ex07/AppCompatActivity.java
@@ -0,0 +1,9 @@
+package com.example.ex07;
+
+import android.os.Bundle;
+
+public class AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        
+    }
+}
--- Ex07/app/src/main/java/com/example/ex07/MainActivity.java
+++ Ex07/app/src/main/java/com/example/ex07/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex07;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button btn1 = (Button) findViewById(R.id.btnNext);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(MainActivity.this,ChildActivity.class);
+                startActivity(intent1);
+            }
         });
     }
 }
