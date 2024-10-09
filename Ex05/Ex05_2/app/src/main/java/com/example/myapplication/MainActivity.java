35d40d08f0e256d0530305c5008d89523cd99709
hhiennnvu123 <hienv4434@gmail.com>
2024-10-09 17:32:19 +0700

    create GUI

--- Ex05/Ex05_2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Ex05/Ex05_2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edtA, edtB, edtC, edtKQ;
+    Button btnPT, btnTT, btnThoat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +23,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        edtA = findViewById(R.id.edt_a);
+        edtB = findViewById(R.id.edt_b);
+        edtC = findViewById(R.id.edt_c);
+        edtKQ = findViewById(R.id.edt_kq);
+        btnPT = findViewById(R.id.btn_pt);
+        btnTT = findViewById(R.id.btn_tt);
+        btnThoat = findViewById(R.id.btn_thoat);
     }
 }
