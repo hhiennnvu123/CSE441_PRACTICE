2b1b371c2f67ea1c50d8fa4ad6fb868d261f22fd
hhiennnvu123 <hienv4434@gmail.com>
2024-10-10 13:56:51 +0700

    update2

--- Ex08/Ex08_1/app/src/main/java/com/example/ex08/MainActivity.java
+++ Ex08/Ex08_1/app/src/main/java/com/example/ex08/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex08;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btncallphone,btnsendsms;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btncallphone = (Button) findViewById(R.id.btncallphone);
+        btnsendsms = (Button) findViewById(R.id.btnsendsms);
+
+        btnsendsms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(MainActivity.this, CallPhoneActivity.class);
+                startActivity(intent1);
+            }
         });
     }
 }
