c18af5df8fe2bed0f9fd952ff1b0f4bab52e5af7
hhiennnvu123 <152370161+hhiennnvu123@users.noreply.github.com>
2024-10-16 21:32:16 +0700

    update2

--- Ex13/Ex13_01/app/src/main/java/com/example/bai1/MainActivity.java
+++ Ex13/Ex13_01/app/src/main/java/com/example/bai1/MainActivity.java
@@ -1,24 +1,26 @@
 package com.example.bai1;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
-
+    TextView selection;
+    AutoCompleteTextView singleComplete;
+    MultiAutoCompleteTextView multiComplete;
+    String[] arr = {"Hà Nội","Huế","Hưng yên","Hà Giang","Hồ Chí Minh","Sài Gòn","Thái Bình","Lâm Đồng"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        selection = (TextView) findViewById(R.id.selection);
+        singleComplete = (AutoCompleteTextView) findViewById(R.id.editauto);
+        ArrayAdapter myadapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1,arr
         });
     }
 }
